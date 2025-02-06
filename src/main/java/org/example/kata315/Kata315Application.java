0ca4921f7fb897df06948c1283fa920bd78933df
iUsup <yusub1988@gmail.com>
2025-02-06 09:28:57 +0300

    Add: Project

---
+++ src/main/java/org/example/kata315/Kata315Application.java
@@ -0,0 +1,13 @@
+package org.example.kata315;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kata315Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kata315Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kata315/Kata315ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kata315;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kata315ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
